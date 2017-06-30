bad8d74f2defef0e84463b979f37693f35a41178
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-30 11:53:52 -0700

    attempting to pair data from multiple APIs in firebase

--- core.js
+++ core.js
@@ -16,7 +16,10 @@ var queryURL;
 var keyword;
 var category;
 var location;
-var queryURL;
+// var queryURL;
+var movieTitle;
+var x = 0;
+var poster = "";
 
 // var eventful = "http://api.eventful.com/json/events/search?" + searchParams + "&app_key=TrvGWQVsBrMhNwnd"
 // var movieDB = "https://api.themoviedb.org/3/movie/" + "?api_key=63f47afce4d3b7ed9971fafd26dc56ac"
@@ -24,24 +27,67 @@ var queryURL;
 $(document).ready(nowPlaying);
 
 function nowPlaying() {
+  // pull currently playing movies from themoviedb
   $.ajax({
     url : "https://api.themoviedb.org/3/movie/now_playing?region=US&api_key=63f47afce4d3b7ed9971fafd26dc56ac",
     method : "GET"
   }).done(function(res){
     var response = res.results;
     for (var i = 0; i < response.length; i++){
-      var title = response[i].title;
-      console.log(title)
-      var titleDisplay = $('<td>').append(title);
+      movieTitle = response[i].title;
 
-      var movieDisplay = $('<tr>');
-      movieDisplay.append(titleDisplay)
-      $('#movie-schedule').append(movieDisplay)
+      database.ref().child('nowPlaying').push({
+        title : movieTitle,
+        poster : poster
+      })
+      // pull posters from omdb
+      $.ajax({
+        url : "http://omdbapi.com/?apikey=40e9cece&t=" + movieTitle,
+        method : "GET"
+      }).done(function(results){
+        // console.log("pulling")
+        var posterURL = results.Poster;
+        console.log(posterURL)
+        database.ref("nowPlaying").update({
+          poster : posterURL
+        })
+      })
     }
-
   })
 }
+// pull data from firebase and append
 
+      // poster.attr('id', 'poster' + y)
+      // poster.addClass('img')
+      // poster.addClass('img-responsive')
+      // console.log(res.Poster);
+      // var posterDisplay = $('<td>').append(poster);
+      // movieDisplay.append(poster);
+
+      // var movieDisplay = $('<tr>').attr('id', 'movie-' + i);
+
+      // console.log(movieTitle)
+      // var titleDisplay = $('<td>').append(movieTitle);
+
+      // // var movieDisplay = $('<tr>');
+      // movieDisplay.append(titleDisplay)
+
+      // $('#movie-schedule').append(movieDisplay)
+      // x++
+    // }
+
+//   })
+
+
+// function getMoviePoster(){
+  // $.ajax({
+  //   url : "http://omdbapi.com/?apikey=40e9cece&t=wonder+woman",
+  //   method : "GET"
+  // }).done(function(res){
+  //   console.log(res.Poster)
+  //   console.log("hello")
+  // })
+// }
 // $.ajax({
 //   url : queryURL,
 //   method : "GET"
@@ -50,5 +96,7 @@ function nowPlaying() {
 // })
 
 // use scrollspy on results list
+
+// $(document).ready(getMoviePoster)
 
 $('body').scrollspy({ target: '#now-playing' })
