0c9e3aebfb8c2ea2cf4cc13a16ddb134636cbbdc
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-01 15:14:52 -0700

    set midnight reset, testing pulling more accurate id for search

--- core.js
+++ core.js
@@ -13,12 +13,15 @@ var database = firebase.database();
 
 var genre;
 var queryURL;
-var keyword;
+var keyword = "";
 var category;
 var location;
+var currentMovies = "now_playing?region=US&";
+// var apiArray = [];
 
 // var queryURL;
 var movieTitle = [];
+// var movieTitle = {};
 var x = 0;
 var posterArray = [];
 
@@ -31,33 +34,74 @@ $(document).ready(getMoviePoster);
 
 database.ref('nowPlaying').on('child_added', nowPlaying);
 
+// reset currently playing movies at midnight
+if (moment() === moment().startOf('day')){
+  dataClear;
+}
+
+
+// database reset
+function dataClear(){
+  database.ref('nowPlaying').remove();
+};
+
 function getMoviePoster() {
+  if (keyword === ""){
+    keyword = currentMovies;
+  }
+
   // pull currently playing movies from themoviedb
   $.ajax({
-    url : "https://api.themoviedb.org/3/movie/now_playing?region=US&api_key=63f47afce4d3b7ed9971fafd26dc56ac",
+    url : "https://api.themoviedb.org/3/movie/" + keyword + "&api_key=63f47afce4d3b7ed9971fafd26dc56ac",
     method : "GET"
   }).done(function(res){
-
     var response = res.results;
+    console.log(response)
     for (var i = 0; i < response.length; i++){
-      var title = response[i].title;
+      var title = {};
+      title.name = response[i].title;
+      title.id = response[i].id;
+      title.imdbID = "";
       movieTitle.push(title);
+      // title.name = title;
+      // title.id = response[i].id;
     }
+    console.log(movieTitle)
+    for (var i = 0; i < movieTitle.length; i++){
+      console.log(x)
+      var id = movieTitle[i].id;
+      // var item = movieTitle[i]
+      // console.log(movieTitle[i].id)
+      $.ajax({
+        url : "https://api.themoviedb.org/3/movie/" + id + "?api_key=63f47afce4d3b7ed9971fafd26dc56ac",
+        method : "GET"
+      }).done(function(res){
+        // console.log(res)
+        var imdbID = res.imdb_id;
+        // console.log(imdbID)
+        console.log(movieTitle)
+        movieTitle[x].imdbID = imdbID;
+      })
+      x++
+    }
+
     // pull info from omdb and log to firebase
     for (var i = 0; i < movieTitle.length; i++){
-      var title = movieTitle[i];
-      console.log(title);
+      var name = movieTitle[i].name;
+      // var imdbID = movieTitle[i].imdbID
+      // console.log(name);
       $.ajax({
-        url : "http://omdbapi.com/?apikey=40e9cece&t=" + title,
+        url : "http://omdbapi.com/?apikey=40e9cece&t=" + name + "&y=" + moment().year(),
         method : "GET"
       }).done(function(results){
-        console.log(results)
+        // console.log(results)
         var posterURL = results.Poster;
-        console.log(posterURL)
+        // console.log(posterURL)
         var newTitle = results.Title
         database.ref().child("nowPlaying/" + newTitle).update({
           title: newTitle,
           poster : posterURL,
+          // id : imdbID
         })
       })
     }
@@ -69,7 +113,7 @@ function nowPlaying(snap, prevChildKey){
   // getMoviePoster;
   var movieDisplay = $('<tr>');
   // create image data for table
-  console.log(snap.val().title)
+  // console.log(snap.val().title)
   var moviePoster = $('<img>').attr('src', snap.val().poster);
   moviePoster.addClass('img img-responsive');
   var displayPoster = $('<td>').append(moviePoster);
@@ -79,3 +123,10 @@ function nowPlaying(snap, prevChildKey){
   movieDisplay.append(displayPoster, titleDisplay);
   $('#movie-schedule').append(movieDisplay);
 }
+
+// function movieSearch(){
+//   keyword = input.val().trim
+//   .$ajax({
+//     url: 
+//   })
+// }
